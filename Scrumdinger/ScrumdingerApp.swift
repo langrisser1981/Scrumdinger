d221d3f5ba9d92f00e94285e4a82c54e8417c34c
lenny_chenh <langrisser1981@gmail.com>
2021-01-05 11:17:35 +0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Lenny Cheng on 2021/1/5.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
